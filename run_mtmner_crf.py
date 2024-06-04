59b7acdf875fdb051471c2c58029b6af5c464974
nhatmin2002 <134791579+nhatmin2002@users.noreply.github.com>
2024-06-05 00:27:33 +0700

    Update run_mtmner_crf.py

--- run_mtmner_crf.py
+++ run_mtmner_crf.py
@@ -585,7 +585,7 @@ def main():
 
     #'''
     if args.task_name == "twitter2015":
-        args.num_train_epochs = 10.0
+        args.num_train_epochs = 1.0
     if args.task_name == "twitter2017":
         args.num_train_epochs = 22.0
     #'''
